1c475113ebf438bcc8b5a320acb1ffbf29266de6
jerem <jerem@LAPTOP-IJFJAN31>
2020-01-24 16:07:54 +0100

    generate player

--- WCS/src/generate/GeneratePlayer.java
+++ WCS/src/generate/GeneratePlayer.java
@@ -1,6 +1,74 @@
 package generate;
 
+import java.util.Random;
+
+import data.Player;
+
 public class GeneratePlayer {
+	private Player playerCreated;
+	private String name;
+	private int attack;
+	private int shot2Pts;
+	private int shot3Pts;
+	private int freeThrows;
+	private int pass;
+	private int ballHandle;
+	private int reboundOff;
+	private int defense;
+	private int block;
+	private int steal;
+	private int reboundDef;
+	private int strenght;
+	private int reactionTime;
+	private int speed;
+	
+	
+	public GeneratePlayer() {
+	
+	}
+	
+	public Player generatePlayer(String name) {
+		this.name=name;
+		this.attack=generateValueAttack();
+		this.shot2Pts=generateValueAttack();
+		this.shot3Pts=generateValueAttack();
+		this.freeThrows=generateValueAttack();
+		this.pass=generateValueAttack();
+		this.ballHandle=generateValueAttack();
+		this.reboundOff=generateValueDefense();
+		this.defense=generateValueDefense();
+		this.block=generateValueDefense();
+		this.steal=generateValueDefense();
+		this.reboundDef=generateValueDefense();
+		this.strenght=generateValueDefense();
+		this.reactionTime=generateValueAttack();
+		this.speed=generateValueAttack();
+		
+		this.playerCreated=new Player(this.name,attack,shot2Pts,shot3Pts,freeThrows,pass,ballHandle,reboundOff,defense,block,steal,reboundDef,strenght
+				,reactionTime,speed);
+		
+		return playerCreated;
+	}
+	
+	public int generateValueAttack() {
+		Random random = new Random();
+		int valueMin=50;
+		int valueMax=100;
+		int value = valueMin + random.nextInt(valueMax - valueMin);
+		return value;
+	}
+	
+	
+	public int generateValueDefense() {
+		Random random = new Random();
+		int valueMin=0;
+		int valueMax=50;
+		int value = valueMin + random.nextInt(valueMax - valueMin);
+		return value;
+	}
+	
+	
+	
 	
 	
 
